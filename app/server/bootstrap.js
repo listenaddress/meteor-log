5727848759415d3ec639a330ee8cec7c29c32c29
Malek Hakim <hakimelek@gmail.com>
2016-07-06 23:50:50 -0500

    support for now and update from user page

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,13 +1,2 @@
 Meteor.startup(function () {
 });
-
-ServiceConfiguration.configurations.upsert(
-  { service: 'meteor-developer' },
-  {
-    $set: {
-      clientId: process.env['ACCOUNTS_METEOR_ID'],
-      secret: process.env['ACCOUNTS_METEOR_SECRET'],
-      loginStyle: 'popup'
-    }
-  }
-);
