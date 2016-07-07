d0324317586a002eae3157f45943a5cc46e1e5f0
Malek Hakim <hakimelek@gmail.com>
2016-07-07 01:20:25 -0500

    add support for now on external html

--- app/server/publish.js
+++ app/server/publish.js
@@ -86,6 +86,12 @@ Meteor.publish('UserNow', function (userId) {
   return Nows.find({userId: userId}, {sort: {createdAt: -1}, limit: 1})
 });
 
+Meteor.publish('myNow', function (username) {
+
+  var userId = Meteor.users.findOne({username: username}, {fields: {profile: 1}});
+  return Nows.find({userId: userId._id}, {sort: {createdAt: -1}, limit: 1})
+});
+
 
 Meteor.publish('now', function (id) {
   var now = Nows.findOne({_id: id});
