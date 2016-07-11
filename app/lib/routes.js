01a5fb917d9d64aa46526665d58d3b63d5877df0
Thomas O'Brien <tbobrien612@gmail.com>
2016-07-10 21:28:55 -0400

    Routes

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -1,90 +1,98 @@
-Router.configure({
-  layoutTemplate: 'MasterLayout',
-  loadingTemplate: 'Loading',
-  notFoundTemplate: 'NotFound'
-});
+if (Meteor.isClient) {
+  Router.configure({
+    layoutTemplate: 'MasterLayout',
+    loadingTemplate: 'Loading',
+    notFoundTemplate: 'NotFound'
+  });
 
-Router.route('/', {
-  name: 'home',
-  controller: 'HomeController',
-  where: 'client'
-});
+  Router.route('/', {
+    name: 'home',
+    controller: 'HomeController',
+    where: 'client'
+  });
 
-Router.route('/now', {
-  name: 'now.list',
-  controller: 'HomeController',
-  action: 'listNows',
-  where: 'client'
-});
+  Router.route('/now', {
+    name: 'now.list',
+    controller: 'HomeController',
+    action: 'listNows',
+    where: 'client'
+  });
 
-Router.route('/note/:_id', {
-  name: 'note',
-  controller: 'NoteController',
-  action: 'detail',
-  where: 'client'
-});
+  Router.route('/note/:_id', {
+    name: 'note',
+    controller: 'NoteController',
+    action: 'detail',
+    where: 'client'
+  });
 
-Router.route('/note/:_id/edit', {
-  name: 'note.edit',
-  controller: 'NoteController',
-  action: 'edit',
-  where: 'client'
-});
+  Router.route('/note/:_id/edit', {
+    name: 'note.edit',
+    controller: 'NoteController',
+    action: 'edit',
+    where: 'client'
+  });
 
-Router.route('/now/:_id', {
-  name: 'now.detail',
-  controller: 'NowController',
-  action: 'detail',
-  where: 'client'
-});
+  Router.route('/now/:_id', {
+    name: 'now.detail',
+    controller: 'NowController',
+    action: 'detail',
+    where: 'client'
+  });
 
-Router.route('/now/:_id/edit', {
-  name: 'now.edit',
-  controller: 'NowController',
-  action: 'edit',
-  where: 'client'
-});
+  Router.route('/now/:_id/edit', {
+    name: 'now.edit',
+    controller: 'NowController',
+    action: 'edit',
+    where: 'client'
+  });
+
+  Router.route('/integration/github', {
+    name: 'integration.github',
+    controller: 'IntegrationController',
+    action: 'detail',
+    where: 'client'
+  });
+
+  Router.route('/account', {
+    before: function () {
+      console.log('yo');
+       if (Meteor.user()) {
+         this.redirect('/');
+       }
+       else{
+         this.render('account');
+       }
+   },
+    name: 'account'
+  });
+
+  // Router.onBeforeAction(function() {
+  //     if (!Meteor.user())
+  //         this.redirect('/account');
+  // }, {except: ['account', 'home', 'user']});
 
 
+  Router.route('/:username', {
+    name: 'user',
+    controller: 'UserController',
+    action: 'detail',
+    where: 'client'
+  });
 
-Router.route('/account', {
-  before: function () {
-     if (Meteor.user()) {
-       this.redirect('/');
-     }
-     else{
-       this.render('account');
-     }
- },
-  name: 'account'
-});
+  Router.route('/:username/edit', {
+    name: 'user.edit',
+    controller: 'UserController',
+    action: 'edit',
+    where: 'client'
+  });
 
-Router.onBeforeAction(function() {
-    if (!Meteor.user())
-        this.redirect('/account');
-}, {except: ['account', 'home', 'user']});
-
-
-Router.route('/:username', {
-  name: 'user',
-  controller: 'UserController',
-  action: 'detail',
-  where: 'client'
-});
-
-Router.route('/:username/edit', {
-  name: 'user.edit',
-  controller: 'UserController',
-  action: 'edit',
-  where: 'client'
-});
-
-Router.route('/:username/history', {
-  name: 'now.history',
-  controller: 'UserController',
-  action: 'edit',
-  where: 'client'
-});
+  Router.route('/:username/history', {
+    name: 'now.history',
+    controller: 'UserController',
+    action: 'edit',
+    where: 'client'
+  });
+}
 
 // Router.route('/user/:_id/notifications', {
 //   name: 'notifications',
@@ -93,4 +101,8 @@ Router.route('/:username/history', {
 //   where: 'client'
 // });
 
-
+Router.route( "/integrations/:integrationId", { where: "server" } )
+  .post(function() {
+    console.log(this.request.body);
+    this.response.end('yo yo');
+  });
