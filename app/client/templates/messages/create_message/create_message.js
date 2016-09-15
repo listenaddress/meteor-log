6931657f51002d30b7de96fda90e6f00a0aeab3d
Thomas O'Brien <tbobrien612@gmail.com>
2016-09-14 23:21:17 -0500

    ADD ABILITY TO ADD USERNAME TO MSG BY CHOOSING ONE FROM LIST. SHOUTS OUT HERSHEY'S CO, WALMART, FASTENAL, ETC

--- app/client/templates/messages/create_message/create_message.js
+++ app/client/templates/messages/create_message/create_message.js
@@ -1,7 +1,16 @@
 Template.CreateMessage.events({
   'keyup textarea.message-input': function (e, tmpl) {
     var message = tmpl.find('.message-input').value;
+    if (!message) return;
     Session.set('message', message);
+    var taggingUserPattern = /\B@[a-z0-9_-]+$/;
+    var match = message.match(taggingUserPattern);
+    if (match) {
+      handleTagging();
+    } else {
+      Session.set('tagging', false);
+    }
+
     if (e.which !== 13) return;
     e.preventDefault();
 
@@ -16,37 +25,46 @@ Template.CreateMessage.events({
         $('.events-list').scrollTop(100000);
       }, 200);
     });
+
+    function handleTagging() {
+      var username = match[0].slice(1);
+      var query = '.*' + username + '.*';
+      Session.set('match', match);
+      Session.set('tagging', true);
+      Session.set('loadingUsers', true);
+      Meteor.subscribe('usersByUsername', query, function () {
+        var users = Meteor.users.find({username: {$regex: query}}).fetch();
+        Session.set('usersToTag', users);
+        Session.set('loadingUsers', false);
+      });
+    }
   },
 
   'click .addIntegration': function (e, tmpl) {
     var controller = Router.current();
     Router.go('log.integrations', { logId: controller.params.logId });
+  },
+
+  'click .user-list li': function (e, tmpl) {
+    var username = e.target.dataset.value;
+    var message = tmpl.find('.message-input').value;
+    var match = Session.get('match');
+
+    message = message.slice(0, -(match[0].length - 1));
+    tmpl.find('.message-input').value = message += username + ' ';
+    tmpl.find('.message-input').focus();
+    Session.set('tagging', false);
   }
 });
 
 Template.CreateMessage.helpers({
-  tagging: function () {
-    var message = Session.get('message');
-    if (!message) return;
-    var taggingUserPattern = /\B@[a-z0-9_-]+$/;
-    var match = message.match(taggingUserPattern);
-    if (!match) return;
-
-    var username = match[0].slice(1);
-    var query = '.*' + username + '.*';
-    Session.set('loadingUsers', true);
-    Meteor.subscribe('usersByUsername', query, function () {
-      var users = Meteor.users.find({username: {$regex: query}}).fetch();
-      Session.set('usersToTag', users);
-      Session.set('loadingUsers', false);
-    });
-
-    return match;
-  },
   loadingUsers: function () {
     return Session.get('loadingUsers');
   },
   usersToTag: function () {
     return Session.get('usersToTag');
+  },
+  tagging: function () {
+    return Session.get('tagging');
   }
 });
