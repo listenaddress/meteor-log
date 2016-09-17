9a41420efaddfbc996b783a90e893650992d0b2c
Thomas O'Brien <tbobrien612@gmail.com>
2016-09-16 23:47:40 -0500

    Fix check for new notification

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -1,35 +1,35 @@
 Template.EventsList.onRendered(function () {
-  setTimeout(function () {
-    var query = Notifications.find({userId: Meteor.userId(), unseen: true});
-    var count = Notifications.find({userId: Meteor.userId(), unseen: true}).count();
+  var query = Notifications.find({userId: Meteor.userId(), unseen: true});
+  var count = Notifications.find({userId: Meteor.userId(), unseen: true}).count();
 
-    // eslint-disable-next-line no-unused-vars
-    var handle = query.observeChanges({
-      // Check if we're tagged in a new message. Play realtalk.mp3 if we are.
-      added: function (id, notification) {
-        var newCount = Notifications.find({userId: Meteor.userId(), unseen: true}).count();
-        if (newCount <= count) return;
+  // eslint-disable-next-line no-unused-vars
+  var handle = query.observeChanges({
+    // Check if we're tagged in a new message. Play realtalk.mp3 if we are.
+    added: function (id, notification) {
+      var now = moment();
+      var then = moment.utc(notification.createdAt);
+      var diff = now.diff(then);
+      if (diff > 20000) return;
 
-        setTimeout(function () {
-          var event = Events.findOne(notification.eventId);
-          if (!event) return;
-          var message = Messages.findOne(event.messageId);
-          if (!message) return;
-          var usersTaggedPattern = /\B@[a-z0-9_-]+/g;
-          var matches = message.content.match(usersTaggedPattern);
-          if (!matches || matches.length < 1) return;
+      setTimeout(function () {
+        var event = Events.findOne(notification.eventId);
+        if (!event) return;
+        var message = Messages.findOne(event.messageId);
+        if (!message) return;
+        var usersTaggedPattern = /\B@[a-z0-9_-]+/g;
+        var matches = message.content.match(usersTaggedPattern);
+        if (!matches || matches.length < 1) return;
 
-          _.map(matches, function (item) {
-            var username = item.replace('@', '');
-            var user = Meteor.users.findOne({username: username});
-            if (user._id !== Meteor.userId()) return;
-            var audio = new Audio('/assets/realtalk.mp3');
-            audio.play();
-          });
-        }, 300);
-      }
-    });
-  }, 5000);
+        _.map(matches, function (item) {
+          var username = item.replace('@', '');
+          var user = Meteor.users.findOne({username: username});
+          if (user._id !== Meteor.userId()) return;
+          var audio = new Audio('/assets/realtalk.mp3');
+          audio.play();
+        });
+      }, 300);
+    }
+  });
 });
 
 Template.MasterLayout.helpers({
