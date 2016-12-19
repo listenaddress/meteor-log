4c1ea5312ed6e1e839bff6716e5de2d52987058e
Thomas O'Brien <tbobrien612@gmail.com>
2016-12-18 23:16:07 -0500

    Add greatest security implementation of all time

--- app/client/templates/logs/log_info/log_info.js
+++ app/client/templates/logs/log_info/log_info.js
@@ -12,7 +12,11 @@ Template.LogInfo.helpers({
 
     if (member) return true;
     else return false;
-  }
+  },
+
+  currentUser: function () {
+    return Meteor.user();
+  },
 });
 
 Template.LogInfo.events({
