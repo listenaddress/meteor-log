9ea823ba4ecc95ee1eb8b0c17cbdf3b5d1616673
Thomas O'Brien <tbobrien612@gmail.com>
2016-12-26 23:59:36 -0500

    Implement some more of the design

--- app/client/templates/search/search.js
+++ app/client/templates/search/search.js
@@ -1,5 +1,3 @@
-var clearResults;
-
 Template.Search.helpers({
   users: function () {
     return Session.get('users');
@@ -13,40 +11,3 @@ Template.Search.helpers({
     return Session.get('messages');
   }
 });
-
-Template.Search.events({
-
-});
-
-Template.SearchBar.events({
-  'click .search-bar': function (e, tmpl) {
-    Router.go('search');
-  },
-
-  'keyup input.search-bar': function (e, tmpl) {
-    var text = tmpl.find('.search-bar').value;
-    if (!text || text === '') return clearResults();
-    var query = '.*' + text + '.*';
-
-    Meteor.subscribe('usersByUsername', query, function () {
-      var users = Meteor.users.find({username: {$regex: query, $options: 'i'}}).fetch();
-      Session.set('users', users);
-    });
-
-    Meteor.subscribe('messagesByText', query, function () {
-      var messages = Messages.find({content: {$regex: query, $options: 'i'}}).fetch();
-      Session.set('messages', messages);
-    });
-
-    Meteor.subscribe('logsByName', query, function () {
-      var logs = Logs.find({name: {$regex: query, $options: 'i'}}).fetch();
-      Session.set('logs', logs);
-    });
-  }
-});
-
-clearResults = function () {
-  Session.set('users', null);
-  Session.set('logs', null);
-  Session.set('messages', null);
-};
